d67412e334b20299f98110cfbf32a8c815379dd1
Raviteja <125804572+Raviteja0524@users.noreply.github.com>
2023-10-10 19:35:42 +0530

    Add files via upload
    Coding Statement : Write a program to find the Number of digits in an integer
    
    Description
    
    Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
    
    Input : 3241
    
    Output : 4
    
    Input : 6
    
    Output : 1

---
+++ 09.py
@@ -0,0 +1,15 @@
+# Coding Statement : Write a program to find Number of digits in an integer
+
+# Description
+
+# Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
+
+# Input : 3241
+
+# Output : 4
+
+# Input : 6
+
+# Output : 1
+
+print(len(str(input())))
